272e0f7211794f39030bbcee7a6e576deb2771fb
Sam Haiden <samhaiden@Sams-MacBook-Air.local>
2019-02-18 07:26:13 -0800

    new basic file structure for non-wordpress, fully customized site.

---
+++ home-samhaiden/include/header.php
@@ -0,0 +1,29 @@
+
+<html>
+    
+    <head>
+    
+        <title>Sam Haiden</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" media="screen and (min-width: 750px)" href="/css/home.css" />
+
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 749px)" href="/css/home-mobile.css" />
+
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100|Alegreya+Sans:100|Josefin+Sans:100|Lato:100|Quicksand:300,400|Amatic+SC:400|Raleway:200,300,400" rel="stylesheet">
+
+        <link href="https://www.samhaiden.com/wp-content/uploads/2018/09/sicon.png" type="image/png" rel="icon">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <script type="text/javascript" src="/script/mobile-menu.js"></script>
+
+    </head>
+
+</html>
+
---
+++ home-samhaiden/index.php
@@ -0,0 +1,104 @@
+<html>
+
+<head>
+
+    <?php include("include/header.php");?>
+
+</head>
+
+<body>
+
+    <?php include("include/menu.php");?>
+
+    <div id="wrapper" class="wrapper-main">
+
+
+        <div class="one row-one" id="one">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <i class="fa fa-sun-o"></i>
+
+                <h2 class="title">Weather Scraper</h2>
+
+            </a>
+
+        </div>
+
+        <div class="two row-one" id="two">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <i class="fa fa-twitter"></i>
+
+                <h2 class="title">Twitter Feed</h2>
+
+            </a>
+
+        </div>
+
+        <div class="three row-one" id="three">
+
+            <a href="https://www.projects.samhaiden.com/reaction-test.php">
+
+                <i class="fa fa-flash"></i>
+
+                <h2 class="title">Reaction Tester</h2>
+
+            </a>
+
+        </div>
+
+        <div class="four one row-two" id="four">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <i class="fa fa-question-circle-o"></i>
+
+                <h2 class="title">Guessing Game</h2>
+
+            </a>
+
+        </div>
+
+        <div class="two row-two" id="five">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <i class="fa fa-book"></i>
+
+                <h2 class="title">Diary</h2>
+
+            </a>
+
+        </div>
+
+        <div class="three row-two" id="six">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <i class="fa fa-sticky-note-o"></i>
+
+                <h2 class="title">Kanban Board</h2>
+
+            </a>
+
+        </div>
+        
+        <div class="all-columns row-three" id="seven">
+
+            <a href="https://www.projects.samhaiden.com/Projects/PHP/weather-scraper.php">
+
+                <h2 class="title">Kanban Board</h2>
+
+            </a>
+
+        </div>
+
+    </div>
+    
+
+
+</body>
+
+</html>
